c385e05e51f81f171fd7297c8652a14509298b13
Praveen Kumar Chauhan <praveenkumarchauhan2022@gmail.com>
2023-05-28 16:05:12 +0530

    finish nav.js

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,18 +1,19 @@
 import styles from "@/app/styles/navbar.module.css"
 import Link from "next/link";
 import Image from "next/image";
-// import Nav from "@/app/components/Nav";
+import Nav from "@/app/components/Nav";
 
 const Header = () => {
     return (
         <header className={styles.main_header}>
             <div className={styles.navbar_brand}>
+                {/* link tag for link no need to use <a> </a> tag*/}
                 <Link href="/">
+                    {/* adding image using image tag , no need to use anchor tag , height and width is required */}
                     <Image src="/logo.png" alt="my logo image" width={200} height={40}/>
                 </Link>
-
             </div>
-            {/* <Nav/> */}
+            <Nav/>
         </header>
     );
 };
